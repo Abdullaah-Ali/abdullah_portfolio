1ab4671cc22ae32ad427fa482343b641ec8bb997
Syed Khubaib Ali <134156961+Abdullaah-Ali@users.noreply.github.com>
2024-09-15 16:14:51 +0500

    make changes

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
+import AppContext from './AppContext';
+import MainApp from './MainApp';
+import GlobalStyles from './theme/GlobalStyles';
+import { lightTheme, darkTheme } from './theme/themes';
+import { Analytics } from '@vercel/analytics/react'; // Import the Analytics component
+
+function App() {
+  window.matchMedia = null;
+  const darkMode = useDarkMode(true);
+
+  return (
+    <AppContext.Provider value={{ darkMode }}>
+      <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <div className="App">
+          <BrowserRouter>
+            <MainApp />
+            <Analytics /> {}
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
